7ffd3106f6c2b8487550a8f9dfd3457b0c17ca1e
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-06 13:38:34 +0530

    Add project files.

---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿/******************************************************************************
+ *  Purpose: Determines whether or not n is prime.
+ *
+ *
+ *  @author  Chetan Rote
+ *  @version 1.0
+ *  @since   06-11-2020
+ ******************************************************************************/
+using System;
+
+namespace DSLinkedList
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Linked List.");
+        }
+    }
+}
