4c210126871832022fc68e931ab4a4cd54aaf3f2
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-09 08:19:31 +0530

    [Chetan] Added Comments.

--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -21,45 +21,45 @@ namespace DSLinkedList
         {
             Console.WriteLine("Welcome to Data Structure Linked List.");
             ///UC1&2
-            //LinkedList linkedList = new LinkedList();
-            //linkedList.Add(56);
-            //linkedList.Add(30);
-            //linkedList.Add(70);
-            //Console.WriteLine("Values in Linked List:-");
-            //linkedList.Display();
-            /////UC3 Appends element.
-            //linkedList.Append(30);
-            //linkedList.Append(70);
-            /////UC4 Inserting element at given Position.
-            //linkedList.InsertAtGivenPosition(2, 30);
-            //Console.WriteLine("30 inserted Between 56 & 70");
-            //linkedList.Display();
-            /////UC5 Delete first element.
-            //Console.WriteLine("Popped first element " + linkedList.Pop().data + ".\n");
-            //linkedList.Display();
-            /////UC6 Delete last element.
-            //Console.WriteLine("Popped last element " + linkedList.PopLast().data);
-            //linkedList.Display();
-            /////UC7 Ability to search element.
-            //Console.WriteLine("Enter the element to be searched");
-            //int data = Convert.ToInt32(Console.ReadLine());
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            Console.WriteLine("Values in Linked List:-");
+            linkedList.Display();
+            ///UC3 Appends element.
+            linkedList.Append(30);
+            linkedList.Append(70);
+            ///UC4 Inserting element at given Position.
+            linkedList.InsertAtGivenPosition(2, 30);
+            Console.WriteLine("30 inserted Between 56 & 70");
+            linkedList.Display();
+            ///UC5 Delete first element.
+            Console.WriteLine("Popped first element " + linkedList.Pop().data + ".\n");
+            linkedList.Display();
+            ///UC6 Delete last element.
+            Console.WriteLine("Popped last element " + linkedList.PopLast().data);
+            linkedList.Display();
+            ///UC7 Ability to search element.
+            Console.WriteLine("Enter the element to be searched");
+            int data = Convert.ToInt32(Console.ReadLine());
 
-            //int position = linkedList.Search(data);
-            //if (position == 0)
-            //{
-            //    Console.WriteLine("No such element found");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Element found at position " + position);
-            //}
-            /////UC8 Inserting element at middle.
-            //linkedList.InsertNodeAfterParticularNode(40, 30);
-            //linkedList.Display();
-            /////UC9 Ability to delete middle element.
-            //linkedList.DeleteMiddleNode(40);
-            //linkedList.Display();
-            //linkedList.Size();
+            int position = linkedList.Search(data);
+            if (position == 0)
+            {
+                Console.WriteLine("No such element found");
+            }
+            else
+            {
+                Console.WriteLine("Element found at position " + position);
+            }
+            ///UC8 Inserting element at middle.
+            linkedList.InsertNodeAfterParticularNode(40, 30);
+            linkedList.Display();
+            ///UC9 Ability to delete middle element.
+            linkedList.DeleteMiddleNode(40);
+            linkedList.Display();
+            linkedList.Size();
             ///UC10
             SortedLinkedList sortedLinkedList = new SortedLinkedList();
             sortedLinkedList.AddElement(56);
